daef65dce97026b9841f0a38803fcc8d5d17742d
tigra38 <119108325+tigra38@users.noreply.github.com>
2022-12-21 19:15:34 +0800

    Delete message.cpp

--- message.cpp
+++
@@ -1,8 +0,0 @@
-#include "message.h"
-
-Message::Message(std::string From, std::string To, std::string Text) : _mFrom {From}, _mTo {To}, _mText {Text}
-{
-    _mFrom = From;
-    _mTo = To;
-    _mText = Text;
-}
