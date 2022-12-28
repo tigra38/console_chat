d21b0657abe2c5884c850ec73cacafc69f907f61
Aldi586 <axd586@yandex.ru>
2022-12-28 07:43:45 +0300

    Fix2: Startup timestamps moved to Chat::initialMenu()

--- main.cpp
+++ main.cpp
@@ -3,20 +3,12 @@
 #include "user.h"
 #include "chat.h"
 #include "chat.cpp"
-#include <chrono>
-#include <ctime>
 #include <exception>
 
-using namespace std;
-using chrono::system_clock;
+
 
 int main()
 {
-   system_clock::time_point today = system_clock::now();
-   time_t tt;
-   tt = system_clock::to_time_t(today);
-   cout << "Today is: " << ctime(&tt);
-   
     Chat<string> chat1;
     try {
         chat1.initialMenu();
